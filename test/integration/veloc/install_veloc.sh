7d11132fd4a3ad48b774f5882b85fe56c924ca0b
mority <mority@posteo.net>
2021-09-16 13:46:37 +0200

    adds installation script for veloc to be automatically build and installed into integration test build folder when integration tests are build

--- test/integration/veloc/install_veloc.sh
+++ test/integration/veloc/install_veloc.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-git clone -b 'veloc-1.4' --depth 1 https://github.com/ECP-VeloC/veloc.git lib
-cd lib
-./bootstrap.sh
-mkdir build
-./auto-install.py --temp temp build
+git clone -b '1.5' --depth 1 https://github.com/ECP-VeloC/veloc.git lib && cd lib && mkdir install && ./bootstrap.sh && ./auto-install.py install
